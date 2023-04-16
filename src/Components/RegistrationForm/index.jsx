088c72c669f05254075ef057b332aa036d6e371e
Naveen Ravi <gamerkidnav@gmail.com>
2023-04-16 16:50:46 +0530

    ui improvements

--- src/Components/RegistrationForm/index.jsx
+++ src/Components/RegistrationForm/index.jsx
@@ -15,9 +15,12 @@ function RegistrationForm() {
             body.items = body.items.map((item) => item.value)
         }
         instance.post('/admin/register', body)
-            .then((response) => {
-                toast("Applied Successfully")
-                router.push(`/admin/registrationDetails/${response.data.data._id}`)
+            .then(({data}) => {
+                if(!data.data)  toast("Error Occured")
+                else{
+                    router.push(`/admin/registrationDetails/${data.data._id}`)
+                    toast("Applied Successfully")
+                }
             })
             .catch((err) => toast("Error Occured"))
     }
@@ -31,19 +34,19 @@ function RegistrationForm() {
 
     return (
 
-        <form style={{ display: 'flex', flexDirection: "column" }} >
-            <h1>Spot Registration Form</h1>
+        <form style={{ display: 'flex', flexDirection: "column" }} className='bg-white bg-opacity-50 backdrop-filter backdrop-blur-md border-2 border-gray-300 border-opacity-50 rounded-lg p-6 shadow-lg' >
+            <h1 className='mb-8'>Spot Registration Form</h1>
             {
                 ['name', 'college', 'department', 'email', 'phone',].map((item) => {
                     return (
                         <div class="md:flex md:items-center mb-6">
                             <div class="md:w-1/3">
                                 <label >
-                                    {item}
+                                    {item.toUpperCase()}
                                 </label>
                             </div>
                             <div >
-                                <input id="inline-full-name" {...(item !=='email' && { required: true })}  type={() => getType(item)} onChange={(e) => body[item] = e.target.value} />
+                                <input id="inline-full-name"  type={() => getType(item)} onChange={(e) => body[item] = e.target.value} />
                             </div>
                         </div>
                     )
@@ -83,7 +86,7 @@ function RegistrationForm() {
                     )
                 })}
             </div>}
-            <button onClick={(e) => handleRegister(e)} class='submitButton' type='submit'>Register</button>
+            <button onClick={(e) => handleRegister(e)} class='bg-blue-500 bg-opacity-70 backdrop-filter backdrop-blur-lg border-2 border-gray-300 border-opacity-50 rounded-lg px-4 py-2 shadow-lg hover:bg-opacity-80 hover:border-opacity-80 hover:shadow-xl transition-all duration-300' type='submit'>Register</button>
         </form>
 
     )
--- src/pages/admin/registrationDetails/[id].js
+++ src/pages/admin/registrationDetails/[id].js
@@ -40,10 +40,73 @@ function registrationDetails() {
     }
 
     return (
-        <div style={{marginTop:'50px'}}>
-            {data !== null && <div className="h-screen flex flex-col items-center ">
+        <div style={{ marginTop: '50px' }}>
+            {data !== null && <div className=" flex flex-col items-center ">
                 <h1 className="text-2xl font-bold mb-4">Registration Details</h1>
-                <div className="border border-gray-300 rounded-lg p-4">
+                <div class="bg-white rounded shadow-lg p-4 px-4 md:p-8 mb-6">
+                    <div class="grid gap-4 gap-y-2 text-sm grid-cols-1 lg:grid-cols-3">
+                        <div class="text-gray-600">
+                            <p class="font-medium text-lg">Registration Details</p>
+                            <p>Please fill out all the fields.</p>
+                        </div>
+
+                        <div class="lg:col-span-2">
+                            <div class="grid gap-4 gap-y-2 text-sm grid-cols-1 md:grid-cols-5">
+                                <div class="md:col-span-5">
+                                    <label for="full_name">Full Name</label>
+                                    <input id="full_name" class="h-10 border mt-1 rounded px-4 w-full bg-gray-50" value={data.name} disabled />
+                                </div>
+                                <div class="md:col-span-5">
+                                    <label for="address">College</label>
+                                    <input id="address" class="h-10 border mt-1 rounded px-4 w-full bg-gray-50" value={data.college} disabled />
+                                </div>
+
+
+                                <div class="md:col-span-5">
+                                    <label for="email">Phone</label>
+                                    <input id="email" class="h-10 border mt-1 rounded px-4 w-full bg-gray-50" value={data.phone} disabled />
+                                </div>
+
+
+                                <div class="md:col-span-2">
+                                    <label for="city">Email Address</label>
+                                    <input id="city" class="h-10 border mt-1 rounded px-4 w-full bg-gray-50" value={data.email} disabled />
+                                </div>
+
+                                <div class="md:col-span-2">
+                                    <label for="state">Registration ID</label>
+                                    <div class="h-10 bg-gray-50 flex border border-gray-200 rounded items-center mt-1">
+                                        <input id="state" class="px-4 appearance-none outline-none text-gray-800 w-full bg-transparent" value={data.registerId} disabled />
+                                    </div>
+                                </div>
+
+                                <div class="md:col-span-2">
+                                    <label for="country">Event</label>
+                                    <div class="h-10 bg-gray-50 flex border border-gray-200 rounded items-center mt-1">
+                                        <input id="country" class="px-4 appearance-none outline-none text-gray-800 w-full bg-transparent" value={data.event} disabled />
+                                    </div>
+                                </div>
+
+
+                                <div class="md:col-span-5">
+                                    <label for="zipcode">Items</label>
+                                    <input id="zipcode" class="transition-all flex items-center h-10 border mt-1 rounded px-4 w-full bg-gray-50" disabled value={`${data.items.map((item) => ` ${item}  `)} `} />
+                                </div>
+
+                                <div class="md:col-span-4 text-right mt-6">
+                                    {data.valid == true && <div class="inline-flex items-end">
+                                        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded " onClick={() => markAsRead()}>Mark As Read</button>
+                                    </div>}
+                                    <div class="inline-flex items-end pl-5">
+                                        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded " className={`${data.valid?'bg-blue-500 hover:bg-blue-600':'bg-red-500 hover:bg-red-600'} text-white py-2 px-4 rounded mr-4`}> {data.valid === true ? "Valid" : "Invalid"}</button>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {/* <div className="border border-gray-300 rounded-lg p-4">
                     <TextDisplay label="Name" value={data.name} />
                     <TextDisplay label="College" value={data.college} />
                     <TextDisplay label="Department" value={data.department} />
@@ -61,7 +124,7 @@ function registrationDetails() {
                             Mark As Read
                         </button>}
                     </div>
-                </div>
+                </div> */}
             </div>}
         </div>
     )
